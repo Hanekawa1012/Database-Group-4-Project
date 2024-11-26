b9533f263d83b00104113305dd8216ecaa071664
Hanekawa1012 <cdzhj1012@163.com>
2024-11-27 06:59:39 +0800

    fixed search bar(really)

--- search_bar.php
+++ search_bar.php
@@ -2,9 +2,8 @@
     <!-- When this form is submitted, this PHP page is what processes it.
      Search/sort specs are passed to this page through parameters in the URL
      (GET method of passing data to a page). -->
-<<<<<<< HEAD
-<form method="get" action="">
-  <div class="row">
+    <form method="get" action="">
+    <div class="row">
     <div class="col-md-5 pr-0">
       <div class="form-group">
         <label for="keyword" class="sr-only">Search keyword:</label>
@@ -15,49 +14,34 @@
             </span>
           </div>
           <input type="text" class="form-control border-left-0" name= "keyword" id="keyword" placeholder="Search for anything">
-=======
-    <form method="get" action="mylistings.php">
-        <div class="row">
-            <div class="col-md-5 pr-0">
-                <div class="form-group">
-                    <label for="keyword" class="sr-only">Search keyword:</label>
-                    <div class="input-group">
-                        <div class="input-group-prepend">
-                            <span class="input-group-text bg-transparent pr-0 text-muted">
-                                <i class="fa fa-search"></i>
-                            </span>
-                        </div>
-                        <input type="text" class="form-control border-left-0" name="keyword" id="keyword"
-                            placeholder="Search for anything">
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-3 pr-0">
-                <div class="form-group">
-                    <label for="cat" class="sr-only">Search within:</label>
-                    <select class="form-control" name="cat" id="cat">
-                        <option selected value="">All categories</option>
-                        <option value="classA">classA</option>
-                        <option value="classB">classB</option>
-                        <option value="classC">classC</option>
-                    </select>
-                </div>
-            </div>
-            <div class="col-md-3 pr-0">
-                <div class="form-inline">
-                    <label class="mx-2" for="order_by">Sort by:</label>
-                    <select class="form-control" name="order_by" id="order_by">
-                        <option selected value="">Choose one...</option>
-                        <option value="startPrice">Price (low to high)</option>
-                        <option value="startPrice desc">Price (high to low)</option>
-                        <option value="datediff(endDate, now())">Soonest expiry</option>
-                    </select>
-                </div>
-            </div>
-            <div class="col-md-1 px-0">
-                <button type="submit" class="btn btn-primary">Search</button>
-            </div>
->>>>>>> e6781260f1e8efdf0b7e4b8f26d5ea5bc581bb46
         </div>
-    </form>
+      </div>
+    </div>
+    <div class="col-md-3 pr-0">
+      <div class="form-group">
+        <label for="cat" class="sr-only">Search within:</label>
+        <select class="form-control" name="cat" id="cat">
+          <option selected value="">All categories</option>
+          <option value="classA">classA</option>
+          <option value="classB">classB</option>
+          <option value="classC">classC</option>
+        </select>
+      </div>
+    </div>
+    <div class="col-md-3 pr-0">
+      <div class="form-inline">
+        <label class="mx-2" for="order_by">Sort by:</label>
+        <select class="form-control" name="order_by" id="order_by">
+          <option selected value="">Choose one...</option>
+          <option value="startPrice">Price (low to high)</option>
+          <option value="startPrice desc">Price (high to low)</option>
+          <option value="datediff(endDate, now())">Soonest expiry</option>
+        </select>
+      </div>
+    </div>
+    <div class="col-md-1 px-0">
+      <button type="submit" class="btn btn-primary">Search</button>
+    </div>
+  </div>
+  </form>
 </div> <!-- end search specs bar -->
